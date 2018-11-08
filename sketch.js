1a1b7fa85a79a5aa00b8ee3b2eeb567aa4582041
Augusto Ramirez <43079736+augustoramirez46@users.noreply.github.com>
2018-11-08 13:21:26 -0500

    fondo
    a ya se tiene los xumbos

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 let fondo= [];;
 let nave;
+let persor;
 let f = 0;
 let pantalla;
 let bombas;
@@ -9,6 +10,9 @@ let buenos = [];
 let tipo;
 let ruta;
 let k;
+let fondo1 = [];
+let puntos; 
+let vida;
 
 function preload(){
     for (let i = 0; i <241; i++) {
@@ -21,14 +25,21 @@ function preload(){
         }
      }
      }
+     fondo1.push(loadImage("bg1/blsno.png"));
+     fondo1.push(loadImage("bg1/soloCosos" + ".png"));
+
+
 
 }
 function setup() {
     createCanvas(1200, 700);
-    nave = new Personaje(2);
+    persor = 2;
+    nave = new Personaje(persor);
     bombas = new Bomba(1);
     platas = new Plata(2);
-    pantalla = 2;
+    pantalla = 1;
+    puntaje = 0;
+    vida=3;
     obstaculo.push(bombas);
     buenos.push(platas);
 
@@ -37,17 +48,35 @@ function setup() {
 
 function draw() {
  switch(pantalla){
+    case 1:
+     print(":C"+fondo.length);
+     escenario();
+     tint(255, 126);
+     image(fondo1[0], 0, 0);
+     tint(255, 255);
+     image(fondo1[1], 0, 0); 
+
+    break;
 
 
     case 2:
      escenario();  
      bombasAzar();
      dinerosAzar();
+     textSize(30);
+     fill(0, 255, 0);
+     text("PUNTAJE:" + puntaje, 1041, 40)
+     fill(255, 0, 0);
+     text("VIDAS:" + vida, 250, 40)     
      for(i=0; i < obstaculo.length; i++){
          if(i != null){
          //les males
          obstaculo[i].dibujar();
          obstaculo[i].mover();
+         
+         if (dist(nave.pX, nave.pY,obstaculo[i].pX, obstaculo[i].pY) < 50){
+            quitarVidas();
+            }
          if ((dist(nave.pX, nave.pY,obstaculo[i].pX, obstaculo[i].pY) < 50)||(obstaculo[i].pY > height+200 )){
             obstaculo.splice(i,1);
 
@@ -63,27 +92,35 @@ function draw() {
                 buenos[i].dibujar();
                 buenos[i].mover();
 
+                if (dist(nave.pX, nave.pY,buenos[i].pX, buenos[i].pY) < 50){
+                    darPuntos();
+                    }
+
                  if ((dist(nave.pX, nave.pY,buenos[i].pX, buenos[i].pY) < 50)||(buenos[i].pY > height+200 )){
-                     buenos.splice(i,1);
-        
+                     buenos.splice(i,1);        
                 }
-        //        if(buenos[i].pY > height+200 ){
-         //           buenos.splice(i,1);
-         //       }
+
+
                 }
             }
-    //  for(i=0; i < buenos.length; i++){
-    //     if(i != null){
-    //         if(buenos[i].pY > height+200 ){
-    //            buenos.splice(i,1);
-    //    }
-    //  }
-    // }
+            if(vida < 1){
+                pantalla++
+            }
+
+
 
             nave.dibujar();    
 
 
      break;  
+
+     case 3:
+      background(255);
+      fill(255, 0, 0);
+      text("ENLAMALASAPOPERRO", width/2, height/2);
+
+
+     break;
 
 
 
@@ -98,6 +135,21 @@ function draw() {
 }
 
 function mousePressed(){
+    switch(pantalla){
+        case 1:
+          if((mouseX <= 1108) && (mouseX >= 861) && (mouseY <= 562) && (mouseY >= 405)){
+              pantalla++;
+          }
+        break;
+
+
+
+
+
+
+
+
+    }
 }
 
 function keyPressed(){
@@ -154,4 +206,10 @@ function dinerosAzar(){
      buenos.push(k);
     }
     
+}
+function darPuntos(){
+    puntaje += 50;
+}
+function quitarVidas(){
+    vida -= 1;
 }
